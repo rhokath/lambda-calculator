febc9eddf7b07129ca38647ad86c17c9921a74e0
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-07-16 18:53:26 -0400

    got numbers to work

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,26 @@
+// import React from "react";
+// import { tsPropertySignature } from "@babel/types";
+
+// const NumberButton = () => {
+//   return (
+//     <>
+//   <button>{props.num}<button/>
+//   </>
+//   )
+  
+// };
+
+// export default NumberButton
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+
+const NumberButton = ({number}) => {
+ return (
+   <>
+     {/* Display a button element rendering the data being passed down from the parent container on props */}
+     <button>{number}</button>
+   </>
+  
+ );
 };
+export default NumberButton;
